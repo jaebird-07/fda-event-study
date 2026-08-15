79775ea37564b1a9eda8771f5375ef2254515f0a
Jae-yi Lee <jaeyilee0715@gmail.com>
2026-08-15 20:00:22 +0800

    Sliced window into estimation, pre-event, event, and post-event window

--- test_fetch.py
+++ test_fetch.py
@@ -31,3 +31,26 @@ print(returns)
 
 combined.to_csv("data/azn_gspc_prices.csv")
 returns.to_csv("data/azn_gspc_returns.csv")
+
+event_idx = returns.index.get_loc("2026-07-14")
+print(event_idx)
+
+estimation_window = returns.iloc[6:206]
+print("Estimation window:")
+print(estimation_window.index[0])
+print(estimation_window.index[-1])
+
+pre_event = returns.iloc[206:216]
+print("Pre-event window:")
+print(pre_event.index[0])
+print(pre_event.index[-1])
+
+event_window = returns.iloc[215:218]
+print("Event window:")
+print(event_window.index[0])
+print(event_window.index[-1])
+
+post_event = returns.iloc[218:227]
+print("Post-event window:")
+print(post_event.index[0])
+print(post_event.index[-1])
